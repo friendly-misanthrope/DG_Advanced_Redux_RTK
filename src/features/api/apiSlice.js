678320634a5642cb8dad73e8e77287543446cb9d
friendly-misanthrope <nickthedevguy@gmail.com>
2024-08-28 23:42:52 -0700

    fixed empty endpoints

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,8 +3,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
-  tagTypes: ['Post'],
-  endpoints: (builder) => {
-
-  }
+  tagTypes: ['Posts'],
+  endpoints: (builder) => ({})
 });
