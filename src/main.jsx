4b352850af79c1b002aa8a83fc24db95ccbab998
friendly-misanthrope <nickthedevguy@gmail.com>
2024-08-30 00:09:36 -0700

    replaced fetchPosts with getPosts from extendedApiSlice

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import App from './App.jsx';
 import { store } from './app/store.js';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { extendedApiSlice } from './features/posts/postsSlice.js';
 import { fetchUsers} from './features/users/usersSlice';
-import { fetchPosts } from './features/posts/postsSlice.js';
 import './index.css';
 
 // Immediately fetch users when app loads
+store.dispatch(extendedApiSlice.endpoints.getPosts.initiate());
 store.dispatch(fetchUsers());
-store.dispatch(fetchPosts());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
