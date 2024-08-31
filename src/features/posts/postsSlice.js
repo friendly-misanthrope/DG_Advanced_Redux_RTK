34b25b908e30cdc80ec92b7d2a0dc32df084fddf
friendly-misanthrope <nickthedevguy@gmail.com>
2024-08-30 18:41:21 -0700

    exported create/update/delete mutation hooks

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -118,7 +118,10 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
 
 export const {
   useGetPostsQuery,
-  useGetPostsByUserIdQuery
+  useGetPostsByUserIdQuery,
+  useAddNewPostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation
 } = extendedApiSlice;
 
 export const selectPostsResult = extendedApiSlice.endpoints.getPosts.select();
